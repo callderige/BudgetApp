652439fb6e9dd0806a2833d2eae647fa112f6b9d
Cliff Allderige <ca.allderige@gmail.com>
2018-06-15 14:10:19 -0400

    Add on click listeners for save and cancel buttons

--- app/src/main/java/com/example/cliff/budgetapp/SettingsActivity.java
+++ app/src/main/java/com/example/cliff/budgetapp/SettingsActivity.java
@@ -3,21 +3,51 @@ package com.example.cliff.budgetapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class SettingsActivity extends AppCompatActivity{
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button saveButton = findViewById(R.id.btn_settings_save);
+        saveButton.setOnClickListener(this);
+        Button cancelButton = findViewById(R.id.btn_settings_cancel);
+        cancelButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_settings_cancel: {
+                finishActivity();
+                break;
+            }
+            case R.id.btn_settings_save: {
+                finishActivity();
+                break;
+            }
+            default: {
+                finishActivity();
+                break;
+            }
+        }
     }
 
     @Override
     public void onBackPressed() {
+        finishActivity();
+    }
+
+    public void finishActivity() {
         Intent intent = new Intent(getBaseContext(), MainActivity.class);
         startActivity(intent);
         overridePendingTransition(0,0);
         finish();
     }
+
 }
