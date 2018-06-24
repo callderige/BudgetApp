1f6709ec9c7116b0b9a3d51fca158d016fefb811
Cliff Allderige <ca.allderige@gmail.com>
2018-06-24 12:54:48 -0400

    Create separate Expense class

--- app/src/main/java/com/example/cliff/budgetapp/DatabaseHelper.java
+++ app/src/main/java/com/example/cliff/budgetapp/DatabaseHelper.java
@@ -6,25 +6,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 
 import sqlite.model.Bill;
+import sqlite.model.Expense;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "BudgetDatabase.db";
-
-    private static final String EXPENSES_TABLE_NAME = "expenses";
-    private static final String COLUMN_EXPENSE_ID = "expense_id";
-    private static final String COLUMN_EXPENSE_NAME = "expense_name";
-    private static final String COLUMN_EXPENSE_SPENT = "expense_spent";
-    private static final String COLUMN_EXPENSE_LIMIT = "expense_limit";
-    private static final String COLUMN_EXPENSE_TYPE = "expense_type";
-
-    private static final String CREATE_TABLE_EXPENSES =
-            "CREATE TABLE " + EXPENSES_TABLE_NAME + "("
-                    + COLUMN_EXPENSE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    + COLUMN_EXPENSE_NAME + " TEXT,"
-                    + COLUMN_EXPENSE_SPENT + " NUMERIC,"
-                    + COLUMN_EXPENSE_LIMIT + " NUMERIC,"
-                    + COLUMN_EXPENSE_TYPE + " TEXT,"
-                    + ")";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -32,12 +17,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public void onCreate(SQLiteDatabase database) {
         database.execSQL(Bill.CREATE_TABLE_BILLS);
-        database.execSQL(CREATE_TABLE_EXPENSES);
+        database.execSQL(Expense.CREATE_TABLE_EXPENSES);
     }
 
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
         database.execSQL("DROP TABLE " + Bill.BILLS_TABLE_NAME);
-        database.execSQL("DROP TABLE " + EXPENSES_TABLE_NAME);
+        database.execSQL("DROP TABLE " + Expense.EXPENSES_TABLE_NAME);
     }
 
 }
---
+++ app/src/main/java/sqlite/model/Expense.java
@@ -0,0 +1,51 @@
+package sqlite.model;
+
+public class Expense {
+    public static final String EXPENSES_TABLE_NAME = "expenses";
+    public static final String COLUMN_EXPENSE_ID = "expense_id";
+    public static final String COLUMN_EXPENSE_NAME = "expense_name";
+    public static final String COLUMN_EXPENSE_SPENT = "expense_spent";
+    public static final String COLUMN_EXPENSE_LIMIT = "expense_limit";
+    public static final String COLUMN_EXPENSE_TYPE = "expense_type";
+
+    private int id;
+    private String name;
+    private double spent;
+    private double limit;
+    private String type;
+
+    public static final String CREATE_TABLE_EXPENSES =
+            "CREATE TABLE " + EXPENSES_TABLE_NAME + "("
+                    + COLUMN_EXPENSE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_EXPENSE_NAME + " TEXT,"
+                    + COLUMN_EXPENSE_SPENT + " NUMERIC,"
+                    + COLUMN_EXPENSE_LIMIT + " NUMERIC,"
+                    + COLUMN_EXPENSE_TYPE + " TEXT,"
+                    + ")";
+
+    public Expense() {}
+
+    public Expense(int id, String name, double spent, double limit, String type) {
+        this.id = id;
+        this.name = name;
+        this.spent = spent;
+        this.limit = limit;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getSpent() {
+        return spent;
+    }
+    public double getLimit() {
+        return limit;
+    }
+    public String getType() {
+        return type;
+    }
+}
