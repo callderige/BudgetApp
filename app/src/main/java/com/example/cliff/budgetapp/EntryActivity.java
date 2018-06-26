67531e53dc3a763a48999cdf9d7b2eb087fb18dc
Cliff Allderige <ca.allderige@gmail.com>
2018-06-26 10:19:20 -0400

    Add components for bill and expense entry

--- app/src/main/java/com/example/cliff/budgetapp/EntryActivity.java
+++ app/src/main/java/com/example/cliff/budgetapp/EntryActivity.java
@@ -3,10 +3,19 @@ package com.example.cliff.budgetapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.ViewFlipper;
 
 public class EntryActivity extends AppCompatActivity implements View.OnClickListener {
+    private Spinner mBudgetTypeSpinner;
+    private Spinner mExpenseTypeSpinner;
+    private ViewFlipper mViewFlipper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,18 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_entry);
         //Default title is set to new bill
         setTitle(getString(R.string.app_bar, "New Bill"));
+
+        mViewFlipper = findViewById(R.id.flipper_form);
+
+        mBudgetTypeSpinner = findViewById(R.id.spinner_budget_type);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.budget_type_array, android.R.layout.simple_spinner_dropdown_item);
+        mBudgetTypeSpinner.setAdapter(arrayAdapter);
+        mBudgetTypeSpinner.setOnItemSelectedListener(new onSpinnerItemSelected());
+
+        mExpenseTypeSpinner = findViewById(R.id.spinner_expense_type);
+        ArrayAdapter<CharSequence> arrayAdapter1 = ArrayAdapter.createFromResource(this, R.array.expense_type_array, android.R.layout.simple_spinner_dropdown_item);
+        mExpenseTypeSpinner.setAdapter(arrayAdapter1);
+        mExpenseTypeSpinner.setOnItemSelectedListener(new onSpinnerItemSelected());
 
         Button saveButton = findViewById(R.id.btn_entry_save);
         saveButton.setOnClickListener(this);
@@ -42,6 +63,32 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onBackPressed() {
         finishActivity();
+    }
+
+    public class onSpinnerItemSelected implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+            switch (adapterView.getId()) {
+                case R.id.spinner_budget_type: {
+                    if (position == 0) {
+                        mViewFlipper.setDisplayedChild(0);
+                    } else {
+                        mViewFlipper.setDisplayedChild(1);
+                    }
+                    break;
+                }
+
+                default: {
+
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
     }
 
     private void finishActivity() {
