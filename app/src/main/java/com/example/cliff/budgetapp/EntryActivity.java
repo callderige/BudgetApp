e4080ff5fe301c8e318d9cfd730be43bb8428ff2
Cliff Allderige <ca.allderige@gmail.com>
2018-06-26 17:08:24 -0400

    Add insert to database through UI

--- app/src/main/java/com/example/cliff/budgetapp/EntryActivity.java
+++ app/src/main/java/com/example/cliff/budgetapp/EntryActivity.java
@@ -3,15 +3,20 @@ package com.example.cliff.budgetapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
+import java.util.HashMap;
+
 public class EntryActivity extends AppCompatActivity implements View.OnClickListener {
-    private Spinner mBudgetTypeSpinner;
     private Spinner mExpenseTypeSpinner;
     private ViewFlipper mViewFlipper;
 
@@ -19,15 +24,16 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+
         //Default title is set to new bill
         setTitle(getString(R.string.app_bar, "New Bill"));
 
         mViewFlipper = findViewById(R.id.flipper_form);
 
-        mBudgetTypeSpinner = findViewById(R.id.spinner_budget_type);
+        Spinner BudgetTypeSpinner = findViewById(R.id.spinner_budget_type);
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.budget_type_array, android.R.layout.simple_spinner_dropdown_item);
-        mBudgetTypeSpinner.setAdapter(arrayAdapter);
-        mBudgetTypeSpinner.setOnItemSelectedListener(new onSpinnerItemSelected());
+        BudgetTypeSpinner.setAdapter(arrayAdapter);
+        BudgetTypeSpinner.setOnItemSelectedListener(new onSpinnerItemSelected());
 
         mExpenseTypeSpinner = findViewById(R.id.spinner_expense_type);
         ArrayAdapter<CharSequence> arrayAdapter1 = ArrayAdapter.createFromResource(this, R.array.expense_type_array, android.R.layout.simple_spinner_dropdown_item);
@@ -44,7 +50,15 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_entry_save: {
-                finishActivity();
+                HashMap<String, String> returnedInsertResult = insertIntoDatabase();
+                if (returnedInsertResult.get("status").equals("success")) {
+                    Toast.makeText(getApplicationContext(),
+                            returnedInsertResult.get("success_information"), Toast.LENGTH_LONG).show();
+                    finishActivity();
+                } else if (returnedInsertResult.get("status").equals("failed")){
+                    Toast.makeText(getApplicationContext(),
+                            returnedInsertResult.get("failed_information"), Toast.LENGTH_LONG).show();
+                }
                 break;
             }
             case R.id.btn_entry_cancel: {
@@ -74,8 +88,6 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
                     mViewFlipper.setDisplayedChild(1);
                     setTitle(getString(R.string.app_bar, "New Expense"));
                 }
-            } else if (adapterView.getId() == R.id.spinner_expense_type) {
-                
             }
         }
 
@@ -90,5 +102,128 @@ public class EntryActivity extends AppCompatActivity implements View.OnClickList
         startActivity(intent);
         overridePendingTransition(0,0);
         finish();
+    }
+
+    /**
+     * Returns a HashMap with values explaining the status of the database insert
+     * @return The hashmap containing the results of the database insertion.
+     */
+    private HashMap<String, String> insertIntoDatabase() {
+        HashMap<String, String> resultsToReturn = new HashMap<>();
+
+        switch (mViewFlipper.getDisplayedChild()) {
+            //When mViewFlipper equals 0, the bill form is recorded
+            case 0: {
+                byte passedChecks = 0;
+
+                EditText editText = findViewById(R.id.edit_bill_name);
+                String billName = editText.getText().toString();
+                if (billName.trim().length() > 0) {
+                    passedChecks += 1;
+                } else {
+                    resultsToReturn.put("status", "failed");
+                    resultsToReturn.put("failed_information", "Invalid bill name.");
+                    break;
+                }
+
+                editText = findViewById(R.id.edit_bill_cost);
+                String billCostString = editText.getText().toString();
+                int decimalPlace = billCostString.indexOf('.');
+
+                if (billCostString.length() - decimalPlace <= 3 || decimalPlace == -1) {
+                    passedChecks += 1;
+                } else {
+                    resultsToReturn.put("status", "failed");
+                    resultsToReturn.put("failed_information", "Invalid bill cost.");
+                    break;
+                }
+
+                double billCost;
+                try {
+                    billCost = Float.parseFloat(editText.getText().toString());
+                    passedChecks += 1;
+                } catch (NumberFormatException numberFormatException) {
+                    Log.d("NumberFormatException", numberFormatException.toString());
+                    resultsToReturn.put("status", "failed");
+                    resultsToReturn.put("failed_information", "Invalid bill cost entry");
+                    break;
+                }
+
+                DatePicker datePicker = findViewById(R.id.date_picker_bill_due);
+                String date = datePicker.getYear() + "-" + datePicker.getMonth() + "-" + datePicker.getDayOfMonth();
+
+                if (passedChecks == 3) {
+                    DatabaseHelper databaseHelper = new DatabaseHelper(getBaseContext());
+                    if (databaseHelper.createBill(billName, billCost, 0, date, 0) != -1) {
+                        resultsToReturn.put("status", "success");
+                        resultsToReturn.put("success_information", "New bill created.");
+                    } else {
+                        resultsToReturn.put("status", "failed");
+                        resultsToReturn.put("failed_information", "Error inserting into database");
+                    }
+                }
+
+                break;
+            }
+            //When mViewFlipper equals 1, the expense form is recorded
+            case 1: {
+                byte passedChecks = 0;
+
+                EditText editText = findViewById(R.id.edit_expense_name);
+                String expenseName = editText.getText().toString();
+                if (expenseName.trim().length() > 0) {
+                    passedChecks += 1;
+                } else {
+                    resultsToReturn.put("status", "failed");
+                    resultsToReturn.put("failed_information", "Invalid Expense name.");
+                    break;
+                }
+
+                editText = findViewById(R.id.edit_expense_limit);
+                String expenseLimitString = editText.getText().toString();
+                int decimalPlace = expenseLimitString.indexOf('.');
+
+                if (expenseLimitString.length() - decimalPlace <= 3 || decimalPlace == -1) {
+                    passedChecks += 1;
+                } else {
+                    resultsToReturn.put("status", "failed");
+                    resultsToReturn.put("failed_information", "Invalid expense limit");
+                    break;
+                }
+
+                double expenseLimit;
+                try {
+                    expenseLimit = Float.parseFloat(editText.getText().toString());
+                    passedChecks += 1;
+                } catch (NumberFormatException numberFormatException) {
+                    Log.d("NumberFormatException", numberFormatException.toString());
+                    resultsToReturn.put("status", "failed");
+                    resultsToReturn.put("failed_information", "Invalid expense limit entry");
+                    break;
+                }
+
+                String expenseType = mExpenseTypeSpinner.getSelectedItem().toString();
+
+                if (passedChecks == 3) {
+                    DatabaseHelper databaseHelper = new DatabaseHelper(getBaseContext());
+                    if (databaseHelper.createExpense(expenseName, 0, expenseLimit, expenseType) != -1) {
+                        resultsToReturn.put("status", "success");
+                        resultsToReturn.put("success_information", "New expense created.");
+                    } else {
+                        resultsToReturn.put("status", "failed");
+                        resultsToReturn.put("failed_information", "Error inserting into database");
+                    }
+                }
+
+                break;
+            }
+            default: {
+                resultsToReturn.put("status", "failed");
+                resultsToReturn.put("failed_information", "Could not find correct view");
+                break;
+            }
+        }
+
+        return resultsToReturn;
     }
 }
